dfd305ceec5c2384488e0648020ba01ae780ad3a
Jongsik Jung <jongsik.jung@coinplug.com>
2024-11-18 15:53:34 +0900

    update pyproject.toml

--- write_a_book_with_flows/src/write_a_book_with_flows/main.py
+++ write_a_book_with_flows/src/write_a_book_with_flows/main.py
@@ -5,12 +5,12 @@ from typing import List
 from crewai.flow.flow import Flow, listen, start
 from pydantic import BaseModel
 
-from write_a_book_with_flows.crews.write_book_chapter_crew.write_book_chapter_crew import (
+from .crews.outline_book_crew.outline_crew import OutlineCrew
+from .crews.write_book_chapter_crew.write_book_chapter_crew import (
     WriteBookChapterCrew,
 )
 from write_a_book_with_flows.types import Chapter, ChapterOutline
 
-from .crews.outline_book_crew.outline_crew import OutlineCrew
 
 
 class BookState(BaseModel):
